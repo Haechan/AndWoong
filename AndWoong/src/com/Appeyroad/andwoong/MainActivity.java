0d673169e48b69de1abc490945a5f8fbeecd5d78
Haechan Mike An <demon3517@gmail.com>
2012-07-19 23:45:29 +0900

    did : hold orientation/comment/solve problem in changing scene need : adjust images' size

--- AndWoong/src/com/Appeyroad/andwoong/MainActivity.java
+++ AndWoong/src/com/Appeyroad/andwoong/MainActivity.java
@@ -32,7 +32,8 @@ import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 	private CCGLSurfaceView mGLSurfaceView;
-	SQLiteDatabase db;
+	static SQLiteDatabase db;
+	static Cursor setting;
 	enum SceneIndex {loading, menu, intro, cave, outro, gameover, tutorial, option, sound, developer}
 	static SceneIndex currentScene;
 	static Handler h;
@@ -46,7 +47,7 @@ public class MainActivity extends Activity {
 		
 		SQLiteOpenHelper dbHelper=new DBHelper(this);
 		db=dbHelper.getWritableDatabase();//is final ok?
-        Cursor setting=db.query("woong", null, null, null, null, null, null);
+        setting=db.query("woong", null, null, null, null, null, null);
         setting.moveToPosition(0);
         if(setting.getInt(setting.getColumnIndex("startTime"))==0){
         	setting.close();
@@ -54,8 +55,9 @@ public class MainActivity extends Activity {
         	newValues.put("startTime", System.currentTimeMillis());
     		String[] whereArgs={"0"};//maybe gg
     		db.update("woong", newValues, "startTime = ?", whereArgs);
+            setting=db.query("woong", null, null, null, null, null, null);
+            setting.moveToPosition(0);
         }
-        else setting.close();
         /*
         Cursor setting1=db.query("woong", null, null, null, null, null, null);
         setting1.moveToPosition(0);
@@ -79,7 +81,7 @@ public class MainActivity extends Activity {
 
 		// no effect here because device orientation is controlled by manifest
 		CCDirector.sharedDirector().setDeviceOrientation(
-				CCDirector.kCCDeviceOrientationPortrait);
+				CCDirector.kCCDeviceOrientationLandscapeLeft);
 
 		// show FPS
 		// set false to disable FPS display, but don't delete fps_images.png!!
@@ -120,82 +122,143 @@ public class MainActivity extends Activity {
 		super.onDestroy();
 
 		CCDirector.sharedDirector().end();
+		setting.close();
 		db.close();
 	}
 
 	static class TemplateLayer extends CCLayer {
+		//units called by any methods
 		CCLabel lbl;
 		CCSprite background, bear;
 
+		//method calling scene
 		public static CCScene scene() {
 			CCScene scene = CCScene.node();
 			CCLayer layer = new TemplateLayer();
-
+			
 			scene.addChild(layer);
 
 			return scene;
 		}
 
+		//method calling layer
+		//it will call proper layer by case(by currentScene)
+		//and this part has initial settings(something to do when they are first called) for each scenes
+		//(z=0 : background)
+		//(z>0 : unit)
 		protected TemplateLayer() {
+			//layer for loading scene
 			if(currentScene==SceneIndex.loading){
 				Log.e("LoadingScene", "start");
+				
 				background=CCSprite.sprite("default.png");
 				addChild(background, 0);
-				background.setPosition(CGPoint.ccp(240, 400));
+				background.setPosition(CGPoint.ccp(400, 240));
+				
 				h= new Handler();
 		        h.postDelayed(runLoading, 2000);				
 			}
+			//layer for menu scene
 			else if(currentScene==SceneIndex.menu){
 				Log.e("MenuScene", "start");
+				
 				background=CCSprite.sprite("bgp2.png");
 				addChild(background, 0);
-				background.setPosition(CGPoint.ccp(240, 400));
+				background.setPosition(CGPoint.ccp(400, 240));
+				
+				h= new Handler();
 				h.postDelayed(runLoading, 2000);
 			}
+			//layer for tutorial scene
 			else if(currentScene==SceneIndex.tutorial){}
+			//layer for option scene
 			else if(currentScene==SceneIndex.option){}
+			//layer for about developer scene
 			else if(currentScene==SceneIndex.developer){}
+			//layer for sound setting scene
 			else if(currentScene==SceneIndex.sound){}
+			//layer for game over scene
 			else if(currentScene==SceneIndex.gameover){}
+			//layer for intro scene
 			else if(currentScene==SceneIndex.intro){}
+			//layer for outro scene
 			else if(currentScene==SceneIndex.outro){}
+			//layer for cave scene
+			//this scene is for play game
 			else if(currentScene==SceneIndex.cave){
 				Log.e("CaveScene", "start");
 				this.setIsTouchEnabled(true);
 
 				lbl = CCLabel.makeLabel("Hello World!", "DroidSans", 24);
+				addChild(lbl, 1);
+				lbl.setPosition(CGPoint.make(400, 240));
 				
-				addChild(lbl, 0);
-				lbl.setPosition(CGPoint.make(240, 400));
-				
-				bear=CCSprite.sprite("fps_images.png");
-				addChild(bear,0);
-				bear.setPosition(CGPoint.ccp(120, 600));
-				bear.runAction(CCSequence.actions(CCMoveTo.action(0.1f, CGPoint.ccp(140, 150)), CCMoveTo.action(1, CGPoint.ccp(400, 150))));
+				bear=CCSprite.sprite("woong.png");
+				addChild(bear, 1);
+				//setting bear's position
+		        if(setting.getInt(setting.getColumnIndex("lastTime"))==0 && setting.getInt(setting.getColumnIndex("lastTime"))==0){
+		        	//bear's position when new game is started
+		        	CGPoint startPoint = CGPoint.make(200, 400);
+		        	bear.setPosition(startPoint);
+		        }
+		        else{
+		        	//bear's position when game is re-loaded
+		        }
+		        //setting bear's action
+				bear.schedule("CaveLogic", 1);
 			}
 		}
 
-		@Override
-		public boolean ccTouchesBegan(MotionEvent event) {
-			CGPoint convertedLocation = CCDirector.sharedDirector().convertToGL(CGPoint.make(event.getX(), event.getY()));
-
-			String title = String.format("touch at point(%.2f, %.2f)",
-					convertedLocation.x, convertedLocation.y);
-
-			if (lbl != null) {
-				lbl.setString(title);
-			}
-
-			return CCTouchDispatcher.kEventHandled;
-		}
-		
+		//change loading scene to menu scene
+		//it has function unnecessary later(function : change menu scene to cave scene)
 		Runnable runLoading=new Runnable(){
 			public void run(){
 				if(currentScene==SceneIndex.loading) currentScene=SceneIndex.menu;
-				else if(currentScene==SceneIndex.menu) currentScene=SceneIndex.cave;
+				else if(currentScene==SceneIndex.menu) currentScene=SceneIndex.cave;//remove this later
 				CCDirector.sharedDirector().replaceScene(TemplateLayer.scene());
 			}
 		};
+		
+		//method called when touch began for each case(scene)
+		@Override
+		public boolean ccTouchesBegan(MotionEvent event) {
+			//when touched in menu scene
+			if(currentScene==SceneIndex.menu){}
+			//when touched in tutorial scene
+			else if(currentScene==SceneIndex.tutorial){}
+			//when touched in option scene
+			else if(currentScene==SceneIndex.option){}
+			//when touched in about developer scene
+			else if(currentScene==SceneIndex.developer){}
+			//when touched in sound setting scene
+			else if(currentScene==SceneIndex.sound){}
+			//when touched in game over scene
+			else if(currentScene==SceneIndex.gameover){}
+			//when touched in intro scene
+			else if(currentScene==SceneIndex.intro){}
+			//when touched in outro scene
+			else if(currentScene==SceneIndex.outro){}
+			//when touched in cave scene
+			else if(currentScene==SceneIndex.cave){
+				CGPoint convertedLocation = CCDirector.sharedDirector().convertToGL(CGPoint.make(event.getX(), event.getY()));
+
+				String title = String.format("touch at point(%.2f, %.2f)",
+					convertedLocation.x, convertedLocation.y);
+
+				if (lbl != null) {
+					lbl.setString(title);
+				}
+			}
+			return CCTouchDispatcher.kEventHandled;			
+		}
+		
+		//bear's action
+		//logics in cave
+		public void CaveLogic(){
+			if(bear!=null){
+				
+			}
+		}
 
 	}
 

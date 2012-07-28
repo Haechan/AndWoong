61478916aeb00477a763e8e24d906b8671727d29
Haechan Mike An <demon3517@gmail.com>
2012-07-28 17:29:17 +0900

    did : add sleepItem, wormwood, garlic/bear eat item/relative coordinate need : activate clicked menu/help schedule find logic

--- AndWoong/src/com/Appeyroad/andwoong/MainActivity.java
+++ AndWoong/src/com/Appeyroad/andwoong/MainActivity.java
@@ -3,6 +3,7 @@ package com.Appeyroad.andwoong;
 import java.io.IOException;
 import java.io.InputStream;
 
+import org.cocos2d.actions.interval.CCMoveBy;
 import org.cocos2d.actions.interval.CCMoveTo;
 import org.cocos2d.actions.interval.CCSequence;
 import org.cocos2d.events.CCTouchDispatcher;
@@ -10,10 +11,14 @@ import org.cocos2d.layers.CCLayer;
 import org.cocos2d.nodes.CCDirector;
 import org.cocos2d.nodes.CCLabel;
 import org.cocos2d.layers.CCScene;
+import org.cocos2d.menus.CCMenu;
+import org.cocos2d.menus.CCMenuItem;
+import org.cocos2d.menus.CCMenuItemImage;
 import org.cocos2d.nodes.CCSprite;
 import org.cocos2d.opengl.CCGLSurfaceView;
 import org.cocos2d.types.CGPoint;
 import org.cocos2d.types.CGRect;
+import org.cocos2d.types.CGSize;
 
 import android.app.Activity;
 import android.content.ContentValues;
@@ -127,9 +132,12 @@ public class MainActivity extends Activity {
 	}
 
 	static class TemplateLayer extends CCLayer {
+		//unit pixel, max size  width(x):*800  height(y):*480
+		final CGPoint unitPixel = CGPoint.ccp(CCDirector.sharedDirector().displaySize().width/800, CCDirector.sharedDirector().displaySize().height/480);
 		//units called by any methods
 		CCLabel lbl;
-		CCSprite background, bear;
+		CCMenu menu;
+		CCSprite background, bear, wormwood, garlic, sleepItem;
 
 		//method calling scene
 		public static CCScene scene() {
@@ -153,7 +161,9 @@ public class MainActivity extends Activity {
 				
 				background=CCSprite.sprite("default.png");
 				addChild(background, 0);
-				background.setPosition(CGPoint.ccp(400, 240));
+				background.setPosition(CGPoint.ccp(400*unitPixel.x, 240*unitPixel.y));
+				background.setScaleX(1.6666666f);
+				background.setScaleY(1.25f);
 				
 				h= new Handler();
 		        h.postDelayed(runLoading, 2000);				
@@ -161,13 +171,25 @@ public class MainActivity extends Activity {
 			//layer for menu scene
 			else if(currentScene==SceneIndex.menu){
 				Log.e("MenuScene", "start");
+				this.setIsTouchEnabled(true);
 				
 				background=CCSprite.sprite("bgp2.png");
 				addChild(background, 0);
-				background.setPosition(CGPoint.ccp(400, 240));
+				background.setPosition(CGPoint.ccp(400*unitPixel.x, 240*unitPixel.y));
+				background.setScaleX(1.6666666f);
+				background.setScaleY(1.25f);
 				
-				h= new Handler();
-				h.postDelayed(runLoading, 2000);
+				CCMenuItem newGameBtn = CCMenuItemImage.item("new_game.png", "new_game_sel.png");
+				CCMenuItem loadGameBtn = CCMenuItemImage.item("loadGame.png", "new_game_sel.png");
+				CCMenuItem tutorialBtn = CCMenuItemImage.item("tutorial.png", "tutorial_sel.png");
+				CCMenuItem aboutBtn = CCMenuItemImage.item("about.png", "about_sel.png");
+				menu = CCMenu.menu(newGameBtn, loadGameBtn, tutorialBtn, aboutBtn);
+			    menu.alignItemsVertically();
+			    menu.setPosition(400*unitPixel.x, 240*unitPixel.y);
+			    addChild(menu, 1);
+				
+			    //h= new Handler();
+				//h.postDelayed(runLoading, 2000);
 			}
 			//layer for tutorial scene
 			else if(currentScene==SceneIndex.tutorial){}
@@ -189,23 +211,44 @@ public class MainActivity extends Activity {
 				Log.e("CaveScene", "start");
 				this.setIsTouchEnabled(true);
 
+				background=CCSprite.sprite("night_with_letter.png");
+				addChild(background, 0);
+				background.setPosition(CGPoint.ccp(400*unitPixel.x, 240*unitPixel.y));
+				background.setScaleX(1.6666666f);
+				background.setScaleY(1.25f);
+				
 				lbl = CCLabel.makeLabel("Hello World!", "DroidSans", 24);
 				addChild(lbl, 1);
-				lbl.setPosition(CGPoint.make(400, 240));
+				lbl.setPosition(CGPoint.make(400*unitPixel.x, 240*unitPixel.y));
+				
+				sleepItem=CCSprite.sprite("sleep_item.png");
+				addChild(sleepItem, 2);
+				sleepItem.setPosition(CGPoint.make(610*unitPixel.x, 450*unitPixel.y));
+				sleepItem.setScale(0.58f);
+				
+				wormwood=CCSprite.sprite("wormwood.png");
+				addChild(wormwood, 2);
+				wormwood.setPosition(CGPoint.make(680*unitPixel.x, 450*unitPixel.y));
+				wormwood.setScale(1);
+				
+				garlic=CCSprite.sprite("garlic.png");
+				addChild(garlic, 2);
+				garlic.setPosition(CGPoint.make(750*unitPixel.x, 450*unitPixel.y));
+				garlic.setScale(1);
 				
 				bear=CCSprite.sprite("woong.png");
 				addChild(bear, 1);
 				//setting bear's position
 		        if(setting.getInt(setting.getColumnIndex("lastTime"))==0 && setting.getInt(setting.getColumnIndex("lastTime"))==0){
 		        	//bear's position when new game is started
-		        	CGPoint startPoint = CGPoint.make(200, 400);
+		        	CGPoint startPoint = CGPoint.make(170*unitPixel.x, 130*unitPixel.y);
 		        	bear.setPosition(startPoint);
 		        }
 		        else{
 		        	//bear's position when game is re-loaded
 		        }
 		        //setting bear's action
-				bear.schedule("CaveLogic", 1);
+				//schedule("CaveLogic", 1);
 			}
 		}
 
@@ -214,16 +257,61 @@ public class MainActivity extends Activity {
 		Runnable runLoading=new Runnable(){
 			public void run(){
 				if(currentScene==SceneIndex.loading) currentScene=SceneIndex.menu;
-				else if(currentScene==SceneIndex.menu) currentScene=SceneIndex.cave;//remove this later
+				//else if(currentScene==SceneIndex.menu) currentScene=SceneIndex.cave;//remove this later
 				CCDirector.sharedDirector().replaceScene(TemplateLayer.scene());
 			}
 		};
 		
 		//method called when touch began for each case(scene)
+		//usually used when buttons are touched
 		@Override
 		public boolean ccTouchesBegan(MotionEvent event) {
+			CGPoint touchedPoint = CCDirector.sharedDirector().convertToGL(CGPoint.make(event.getX(), event.getY()));
+			
 			//when touched in menu scene
-			if(currentScene==SceneIndex.menu){}
+			if(currentScene==SceneIndex.menu){
+				Log.e("menu","clicked");
+				
+				//when new game is clicked
+				if(menu.getSelectedItem().getTag()==0){
+					Log.e("menu","new game");
+					currentScene=SceneIndex.cave;
+					CCDirector.sharedDirector().replaceScene(TemplateLayer.scene());
+				}
+				//when load game is clicked
+				else if(menu.getSelectedItem().getTag()==1){
+					Log.e("menu","new game");
+				}
+				//when tutorial is clicked
+				else if(menu.getSelectedItem().getTag()==2){
+					Log.e("menu","new game");
+				}
+				//when about is clicked
+				else if(menu.getSelectedItem().getTag()==3){
+					Log.e("menu","new game");
+				}
+				
+				/*
+				//when new game is clicked
+				if(CGRect.containsPoint(menu.getChildByTag(0).getBoundingBox(), touchedPoint)){
+					Log.e("menu","new game");
+					currentScene=SceneIndex.cave;
+					CCDirector.sharedDirector().replaceScene(TemplateLayer.scene());
+				}
+				//when load game is clicked
+				else if(CGRect.containsPoint(menu.getChildByTag(1).getBoundingBox(), touchedPoint)){
+					Log.e("menu","new game");
+				}
+				//when tutorial is clicked
+				else if(CGRect.containsPoint(menu.getChildByTag(2).getBoundingBox(), touchedPoint)){
+					Log.e("menu","new game");
+				}
+				//when about is clicked
+				else if(CGRect.containsPoint(menu.getChildByTag(3).getBoundingBox(), touchedPoint)){
+					Log.e("menu","new game");
+				}
+				*/
+			}
 			//when touched in tutorial scene
 			else if(currentScene==SceneIndex.tutorial){}
 			//when touched in option scene
@@ -240,10 +328,8 @@ public class MainActivity extends Activity {
 			else if(currentScene==SceneIndex.outro){}
 			//when touched in cave scene
 			else if(currentScene==SceneIndex.cave){
-				CGPoint convertedLocation = CCDirector.sharedDirector().convertToGL(CGPoint.make(event.getX(), event.getY()));
-
 				String title = String.format("touch at point(%.2f, %.2f)",
-					convertedLocation.x, convertedLocation.y);
+						touchedPoint.x, touchedPoint.y);
 
 				if (lbl != null) {
 					lbl.setString(title);
@@ -251,12 +337,57 @@ public class MainActivity extends Activity {
 			}
 			return CCTouchDispatcher.kEventHandled;			
 		}
+
+		//method called when touch moved
+		//usually used when something is dragged
+		//(dragging happens only in cave scene)
+		@Override
+		public boolean ccTouchesMoved(MotionEvent event){
+			if(currentScene==SceneIndex.cave){
+				CGPoint touchedPoint=CCDirector.sharedDirector().convertToGL(CGPoint.ccp(event.getX(), event.getY()));
+				//convertToGL float float CGPoint
+				if(CGRect.containsPoint(garlic.getBoundingBox(), touchedPoint)) garlic.setPosition(touchedPoint);
+				else if(CGRect.containsPoint(wormwood.getBoundingBox(), touchedPoint)) wormwood.setPosition(touchedPoint);
+				else if(CGRect.containsPoint(sleepItem.getBoundingBox(), touchedPoint)) sleepItem.setPosition(touchedPoint);
+			}
+
+			return CCTouchDispatcher.kEventHandled;
+		}
+		
+		//method called when touch moved
+		//usually used when dragging ends
+		//(dragging happens only in cave scene)
+		@Override
+		public boolean ccTouchesEnded(MotionEvent event){
+			if(currentScene==SceneIndex.cave){
+				CGPoint touchedPoint=CCDirector.sharedDirector().convertToGL(CGPoint.ccp(event.getX(), event.getY()));
+				
+				if(CGRect.containsPoint(bear.getBoundingBox(), touchedPoint)){
+					if(CGRect.containsPoint(sleepItem.getBoundingBox(), touchedPoint)){
+						Log.e("bear","sleepItem");
+					}           /*animation happens*/
+					else if(CGRect.containsPoint(wormwood.getBoundingBox(), touchedPoint)){
+						Log.e("bear","wormwood");
+					}           /*animation happens*/  
+					else if(CGRect.containsPoint(garlic.getBoundingBox(), touchedPoint)){
+						Log.e("bear","garlic");
+					}           /*animation happens */
+				}
+				
+				sleepItem.setPosition(CGPoint.make(610*unitPixel.x, 450*unitPixel.y));
+				wormwood.setPosition(CGPoint.make(680*unitPixel.x, 450*unitPixel.y));
+				garlic.setPosition(CGPoint.make(750*unitPixel.x, 450*unitPixel.y));
+			}
+			
+			return CCTouchDispatcher.kEventHandled;
+		}
 		
 		//bear's action
 		//logics in cave
 		public void CaveLogic(){
 			if(bear!=null){
-				
+				bear.runAction(CCMoveBy.action(0.2f, CGPoint.ccp(15*unitPixel.x, 0*unitPixel.y)));
+				Log.e("bear","move");
 			}
 		}
 
